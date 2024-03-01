a29a0a5c835397de9535712bcad97c30f7ac276f
kebabnekaise <emsi06001@utb.vaxjo.se>
2024-03-01 10:39:30 +0100

    Add project files.

---
+++ inluppg6.12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace inluppg6_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv önskad höjd");
+            RitaTriangel(int.Parse(Console.ReadLine()));
+        }
+
+
+        static void RitaTriangel(int h)
+        {
+            int levelSidobredd = h - 1;
+            int levelBredd = 1;
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < levelSidobredd; j++)
+                {
+                    Console.Write(' ');
+                }
+                for (int k = 0; k < levelBredd; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write('\n');
+                levelBredd+=2;
+                levelSidobredd--;
+            }
+        }
+    }
+}
